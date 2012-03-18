b5dc100ba8361c819efa7c305990d7fbfa7f009c
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-17 19:13:06 -0700

    Logging in and logging out work as expected on a basic level.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Indiereel::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "index#index"
+
+  #match "/welcome" => "index#welcome", :as => 'welcome'
+
   match "/auth/facebook/callback" => "sessions#create"
   match "/logout" => "sessions#destroy", :as => 'logout'
 
@@ -50,7 +54,7 @@ Indiereel::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  # just remember to delete public/welcome.html.
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
