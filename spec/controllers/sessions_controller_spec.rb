8b05b7a48e6e623fe8e337ae82ef5a54c557154a
Amy Kuo <amyykuo@berkeley.edu>
2012-03-17 20:28:28 -0700

    starting session, application rspec files and somewhat finished login_out_create_update_users features

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  describe 'create user with omniauth' do
+    'it should'
+  end
+  
+  describe 'logging out a user' do
+  end
+  
+=begin
+# try using shared_examples_for and it_should_behave_like for DRYNESS
+describe ProfileController do
+
+  describe 'index' do
+  end
+  
+  describe 'create a role profile' do
+  end
+  
+  describe 'delete' do
+  end
+  
+  describe 'edit' do
+  end
+=end
+  
 end
