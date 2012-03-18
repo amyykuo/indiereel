a6cea570cc5f38aa36408695eb3d0166eef68fe7
Amy Kuo <amyykuo@berkeley.edu>
2012-03-17 21:29:54 -0700

    finished starting rspec tests for application and session, and cucumber tests for login/logout

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe SessionsController do
-
-  describe 'create user with omniauth' do
-    'it should'
+  
+  it 'should skip the authentication requirement'
+  
+  describe 'create user with omniauth or find registered user' do
+    it 'should log the user into app through facebook'
+    it 'should find or create the user with the info facebook returns'
+    it 'should update the user if need be'
+    it 'should set the establish the correct user is logged in'
+    it 'should redirect to the correct home page'
   end
   
   describe 'logging out a user' do
+    it 'should set the session to nil'
+    it 'should redirect user to the welcome page'
   end
-  
+
 =begin
 # try using shared_examples_for and it_should_behave_like for DRYNESS
 describe ProfileController do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 
 describe User do
 #  pending "add some examples to (or delete) #{__FILE__}"
-
+  describe 'create' do
+  end
+  
+  describe 'update' do
+  end
+  
 end
