270fb22efac0f5593d5338d77c6835f495e6efb6
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-18 18:47:49 -0700

    Updated the roles controller and some routes.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,10 +1,19 @@
 class RolesController < ApplicationController
   def show
     @user = User.find_by_identifier(params[:identifier])
-    @role = params[:role]
+    @role = Role.find_by_type_and_user_id(params[:role], @user.id)
+    if @role.nil?
+      render 'public/404'
+    end
   end
 
   def new
-    render :nothing => true
+    @user = User.find_by_identifier(params[:identifier])
+  end
+  
+  def create
+    type = params[:type]
+    Role.create!(:user => current_user, :type => type)
+    redirect_to "/#{current_user.identifier}/#{type}"
   end
 end
