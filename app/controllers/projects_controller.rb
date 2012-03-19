42bb36f8a46b58b3e13db9d8358503b60c13bfd0
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-18 23:16:44 -0700

    Modified the projects controller to use role objects instead of strings.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
     @user = User.find_by_identifier(params[:identifier])
-    @role = params[:role]
+    @role = Role.find_by_name_and_user_id(params[:role], @user.id)
   end
 end
