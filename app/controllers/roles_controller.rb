07873bb1597540505586bdb9f7a29b94298b80b2
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-21 00:30:16 -0700

    Added a 404 redirect if you attempt to access a user or a role that doesn't exist.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,7 +2,7 @@ class RolesController < ApplicationController
   
   def show
     @user = User.find_by_identifier(params[:identifier])
-    @role = Role.find_by_name_and_user_id(params[:role], @user.id)
+    @role = Role.find_by_name_and_user_id(params[:role], @user.id) rescue nil
     if @role.nil?
       render 'public/404'
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_identifier(params[:identifier])
-    @roles = @user.roles
+    @roles = @user.roles rescue nil
+    
+    render 'public/404' if @user.nil?
   end
 end
