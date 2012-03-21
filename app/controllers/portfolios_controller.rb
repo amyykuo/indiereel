ab1879d9fe9b5d9d28f092055401b7033c86656c
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-21 00:39:03 -0700

    Added 404s for portfolios and projects that don't exist.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   def show
     @user = User.find_by_identifier(params[:identifier])
-    @role = Role.find_by_name_and_user_id(params[:role], @user.id)
+    @role = Role.find_by_name_and_user_id(params[:role], @user.id) rescue nil
+    render 'public/404' if @role.nil?
   end
 end
