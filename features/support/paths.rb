e75107e010a74c3ce56e291e01711fdec06f23f1
Amy Kuo <amyykuo@berkeley.edu>
2012-03-24 18:41:29 -0700

    cucumber tests for home

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,11 +13,14 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
+    when /^the "([^"]*)" home\s?page$/ # user's home page
       '/'
       
     when /^the welcome page$/
       '/' # need to change this eventually
+      
+    when /^"([^"]*)" Edit Role page$/
+      '/'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
