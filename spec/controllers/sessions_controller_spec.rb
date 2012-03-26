7021cfa97f8e4a1bb7ea07766379a4ce0b35d97e
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-25 17:24:14 -0700

    Initial testing for the SessionsController.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,42 +4,57 @@ describe SessionsController do
   
   it 'should skip the authentication requirement'
   
+  
+  # Testing the create action
   describe 'create user with omniauth or find registered user' do
     
+    # Set the request environment variable, and define a mock user
     before :each do
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
       @kunal = mock('Kunal', :uid => "1234", :name => "Kunal", :update_with_omniauth => "lol")
-      User.stub!(:find_by_uid).with("1234").and_return(@kunal)
     end
     
+    # The common functionality of the create method
+    shared_examples_for "the rest of the method" do
+      it 'should update the user with the latest information from Facebook' do
+        @kunal.should_receive(:update_with_omniauth)
+        post :create
+      end
+
+      it 'should set the establish the correct user is logged in' do
+        post :create
+        session[:user_id].should == "1234"
+      end
+
+      it 'should redirect to the correct home page' do
+        post :create
+        response.should redirect_to root_url
+      end
+    end
+    
+    # If a user already exists
     describe 'user exists' do
       it 'should find the user using the Facebook authentication info' do
+        User.stub!(:find_by_uid).with("1234").and_return(@kunal)
         User.should_receive(:find_by_uid).with("1234").and_return(@kunal)
         post :create
       end
+      it_behaves_like "the rest of the method"
     end
     
+    # If a user does not exist
     describe "user doesn't exist" do
       it 'should create the user using the create_with_omniauth model method' do
+        User.stub!(:create_with_omniauth).and_return(@kunal)
+        User.should_receive(:create_with_omniauth).and_return(@kunal)
+        post :create
       end
-    end
-    
-    it 'should update the user with the latest information from Facebook' do
-      @kunal.should_receive(:update_with_omniauth)
-      post :create
-    end
-    
-    it 'should set the establish the correct user is logged in' do
-      post :create
-      session[:user_id].should == "1234"
-    end
-    
-    it 'should redirect to the correct home page' do
-      post :create
-      response.should redirect_to root_url
+      it_behaves_like "the rest of the method"
     end
   end
   
+  
+  # Testing the destroy action
   describe 'logging out a user' do
     it 'should set the session to nil'
     it 'should redirect user to the welcome page'
