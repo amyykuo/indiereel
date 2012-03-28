c7469a1cd1576a80b0e4204383c40f4c3771ce77
Kunal Mehta <k.mehta@berkeley.edu>
2012-03-27 21:04:17 -0700

    Changed more 'name's to 'role_type's. Awesome.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,7 +2,7 @@ class RolesController < ApplicationController
   
   def show
     @user = User.find_by_identifier(params[:identifier])
-    @role = Role.find_by_name_and_user_id(params[:role], @user.id) rescue nil
+    @role = Role.find_by_role_type_and_user_id(params[:role], @user.id) rescue nil
     if @role.nil?
       render 'public/404'
     end
