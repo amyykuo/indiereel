42d98734965cd240cf289d8e9fcaf1482c7c34a6
Kunal Mehta <k.mehta@berkeley.edu>
2012-04-02 16:03:42 -0700

    Trivial commit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     render 'public/404' if @user.nil?
   end
   
+  # This is for the default role portion of the view, to be implemented later.
   def update
     user = User.find_by_identifier(params[:identifier])
     user.default_role = Role.find_by_user_id_and_role_type(@user.id, params[:role_type]) unless params[:role_type].nil?
