1dae5ee1e67a2bb9533eae0ab1ac27c133f20139
Kunal Mehta <k.mehta@berkeley.edu>
2012-04-03 17:03:00 -0700

    Added the before_filter test cases to application_controller_spec.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe ApplicationController do
-  # there's some stuff like protect_from_forgery and filter stuff and I don't know how to test those
+  
+  controller do
+    def index
+      render :nothing => true
+    end
+  end
   
   describe 'getting the current user' do
     describe 'user is registered' do
@@ -19,20 +24,20 @@ describe ApplicationController do
     end
   end
   
-  # we're assuming authenticated_user is called in every controller appropiately
+  # We're assuming authenticated_user is called in every controller appropiately
   describe 'authenticate user / make sure there is a user is logged in' do
-    describe 'current_user is nil' do
-      #it 'should redirect to the welcome page' do
-      #  session[:user_id] = nil
-      #  controller.send(:authentication_required).should redirect_to root_url
-      #end
+    describe 'user is not logged in' do
+      it 'should redirect to the welcome page' do
+        get :index
+        response.should redirect_to root_url
+      end
     end
     describe 'current_user is not nil' do
-      #it 'should redirect to the user\'s home page'
-      #  User.stub!(:find_by_uid).with("1234").and_return(@user = mock("User"))
-      #  session[:user_id] = "1234"
-      #  get 
-        
+      it 'should redirect to the user\'s home page' do
+        login_as("Kunal")
+        get :index
+        response.body.should be_blank
+      end   
     end
   end
   
