9a8866a67ccb9906de37947f00200432983f76ad
Jay Kim <jay.kim@berkeley.edu>
2012-04-12 22:53:29 -0700

    ...did i break something? reverted roles controller prettifying :(

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,12 +1,12 @@
 class RolesController < ApplicationController
   def show
-    @user = User.find_by_identifier params[:identifier]
+    @user = User.find_by_identifier(params[:identifier])
     @role = Role.find_by_role_type_and_user_id(params[:role], @user.id) rescue nil
     render_not_found if @role.nil?
   end
   
   def new
-    @user = User.find_by_identifier current_user.identifier
+    @user = User.find_by_identifier(current_user.identifier)
     @ages = Role.ages
     @options = []
     
@@ -16,24 +16,24 @@ class RolesController < ApplicationController
   end
   
   def create
-    @user = User.find_by_identifier current_user.identifier
+    @user = User.find_by_identifier(current_user.identifier)
     @role_being_created = Role.find_by_role_type_and_user_id(params[:role], @user.id) rescue nil
     
 	  if @role_being_created.nil?
-	    role = Role.create params[:role]
+	    role = Role.create(params[:role])
       role.media_collections << MediaCollection.create_default
       role.save
       
       flash[:notice] = "Role created."
-      redirect_to role_route role
+      redirect_to role_route(role)
     else
       flash[:error] = "There already was a role of that kind."
-      redirect_to home_route current_user
+      redirect_to home_route(current_user)
     end
   end
   
   def edit
-    @user = User.find_by_identifier params[:identifier]
+    @user = User.find_by_identifier(params[:identifier])
     @role = Role.find_by_role_type_and_user_id(params[:role], @user.id) rescue nil
     @ages = Role.ages
     
@@ -41,8 +41,8 @@ class RolesController < ApplicationController
   end
   
   def update
-    @role = Role.find params[:id]
-    @role.update_attributes params[:role]
+    @role = Role.find(params[:id])
+    @role.update_attributes(params[:role])
     
     if @role.valid?
       flash[:notice] = "#{@role.role_type} was successfully updated."
@@ -50,7 +50,7 @@ class RolesController < ApplicationController
       flash[:error] = "There were some errors in updating your role..."
     end
     
-    redirect_to role_route @role
+    redirect_to role_route(@role)
   end
   
   def destroy
