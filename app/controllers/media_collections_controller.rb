04e5cc52ff9f0dddf2532c848207b0cea6158ffe
Jay Kim <jay.kim@berkeley.edu>
2012-04-12 23:57:37 -0700

    roles controller fix; MC controller preliminary setup

--- app/controllers/media_collections_controller.rb
+++ app/controllers/media_collections_controller.rb
@@ -1,2 +1,18 @@
 class MediaCollectionsController < ApplicationController
+  def create
+    #create a new media collection
+  end
+  
+  def update
+    #edit media collection
+    @mc = MediaCollection.find params[:id]
+    #@mc.update_attributes params[:mc]
+    
+    
+    
+  end
+  
+  def destroy
+    #delete media collection
+  end
 end
