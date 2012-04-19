eb3ce86beb7dd98723102fda4c946e0137bd1be2
Kunal Mehta <k.mehta@berkeley.edu>
2012-04-19 14:54:24 -0700

    Removed the before_create from MediaCollection, because before_save takes care of that.

--- app/models/media_collection.rb
+++ app/models/media_collection.rb
@@ -1,11 +1,11 @@
 class MediaCollection < ActiveRecord::Base
+  
   belongs_to :role
   has_many :media_assets, :dependent => :destroy
   
   validates :title, :presence => true
   
   before_save :set_slug
-  before_create :set_slug
   
   def self.create_quickshow(role)
     MediaCollection.create(:title => "QuickShow", :description => "Show off your best work in this album!", :headshot => false, :quickshow => true, :role_id => role.id)
