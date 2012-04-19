17411b98f54af044a42052fa38e7a93f53c4ed01
Amy Kuo <amyykuo@berkeley.edu>
2012-04-18 22:21:44 -0700

    role creation now sets default role if there is only one role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find_by_identifier(params[:identifier])
     if current_user.uid == @user.uid
-      @user = User.find_by_identifier(params[:identifier])
       @roles = @user.roles rescue nil
       @grouped_roles = @user.roles_in_groups_of(2)
       render_not_found if @user.nil?
     else
-      @user = User.find_by_identifier(params[:identifier])
       default_role = Role.find_by_id(@user.default_role_id)
       @role = Role.find_by_role_type_and_user_id(default_role.role_type, @user.id) rescue nil
       render_not_found if @user.nil?
