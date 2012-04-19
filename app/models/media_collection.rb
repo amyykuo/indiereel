7d50a281592384a7c076fd0d07ebc0df38066299
saasbook <saasbook@saasbook.(none)>
2012-04-18 19:27:58 -0700

    in the process of making paperclip work

--- app/models/media_collection.rb
+++ app/models/media_collection.rb
@@ -1,7 +1,9 @@
 class MediaCollection < ActiveRecord::Base
   belongs_to :role
+  has_attached_file :media, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
   validates :title, :presence => true
+  
   
   def self.create_quickshow(role)
     MediaCollection.create(:title => "QuickShow", :description => "Show off your best work in this album!", :headshot => false, :quickshow => true, :role_id => role.id)
