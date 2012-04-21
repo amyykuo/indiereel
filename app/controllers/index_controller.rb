ff8422049a66d4aadd3d88c4a7f3d9c43aabd62c
Kunal Mehta <k.mehta@berkeley.edu>
2012-04-21 03:54:26 -0700

    The IndexController determines layout dynamically so that the user sees a navbar on external pages if they're logged in.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
 
-  skip_before_filter :authentication_required
+  skip_before_filter :authentication_required 
+  layout :determine_layout
 
   def index
       redirect_to home_route(current_user) unless current_user.nil?
@@ -19,6 +20,12 @@ class IndexController < ApplicationController
   end
   
   def help
+  end
+  
+  private
+  
+  def determine_layout
+    current_user ? "application" : "index"
   end
 
 end
