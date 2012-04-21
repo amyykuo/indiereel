844fbfdaff8640492895ac30967c3304735da77a
Kunal Mehta <k.mehta@berkeley.edu>
2012-04-20 23:00:57 -0700

    Added some default role controls to the users/show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_identifier(params[:identifier])
+    render_not_found and return if @user.nil?
     if current_user.uid == @user.uid
       @roles = @user.roles rescue nil
       @grouped_roles = @user.roles_in_groups_of(2)
-      render_not_found if @user.nil?
     else
-      default_role = Role.find_by_id(@user.default_role_id)
-      puts @user.default_role_id
-      @role = Role.find_by_role_type_and_user_id(default_role.role_type, @user.id) rescue nil
-      render_not_found if @user.nil?
+      default_role = @user.default_role
+      render_not_found and return if default_role.nil?
       redirect_to role_route(default_role)
     end
   end
  
-  # This is for the default role portion of the view, to be implemented later.
+  # Only used to update Default Role
   def update
-    user = User.find_by_identifier(params[:identifier])
-    user.default_role = Role.find_by_user_id_and_role_type(@user.id, params[:role_type]) unless params[:role_type].nil?
-    user.save 
+    new_default_role = Role.find_by_id_and_user_id(params[:role_id], current_user.id)
+    if new_default_role.nil?
+      flash[:error] = "There was an error in setting your default role."
+    else
+      user.default_role = new_default_role
+      user.save
+      flash[:notice] = "Default role successfully updated to #{new_default_role.role_type.capitalize}"
+    end
     redirect_to home_route(@user)
   end
 
