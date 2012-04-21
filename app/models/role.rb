f94e0ee71013c7a82050e8e78853e94332ede5b3
Kunal Mehta <k.mehta@berkeley.edu>
2012-04-20 21:42:08 -0700

    Default roles fully working.

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,6 +7,8 @@ class Role < ActiveRecord::Base
   validates :role_name, :presence => true
   validates :role_type, :presence => true
   
+  before_create :check_for_default_role
+  after_destroy :transfer_default_role
   
   def self.options
     ['talent', 'director', 'producer', 'crew']
@@ -25,6 +27,21 @@ class Role < ActiveRecord::Base
   
   def profile_pic_url
     self.profile_pic.file? ? self.profile_pic.url(:profile) : "default_role_pic.jpg"
+  end
+  
+  private 
+  
+  def check_for_default_role
+    self.default_role_user = self.user if self.user.roles.empty?
+  end
+  
+  def transfer_default_role
+    user = self.user
+    new_role = Role.find_all_by_user_id(user.id).first
+    if (not self.default_role_user.nil?) and (not new_role.nil?)
+      user.default_role = new_role
+      user.save
+    end
   end
   
   #def nil_to_empty
