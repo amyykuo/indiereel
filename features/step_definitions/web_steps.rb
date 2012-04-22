c4f744445eed1018095a860dcda5251f4eb0d5ff
Amy Kuo <amyykuo@berkeley.edu>
2012-04-21 18:43:31 -0700

    finished testing for preview, its really bad tho

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -232,6 +232,8 @@ end
  
 Then /^(?:|I )should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
+  puts current_path
+  puts path_to(page_name)
   if current_path.respond_to? :should
     current_path.should == path_to(page_name)
   else
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -43,9 +43,9 @@ module NavigationHelpers
       role = $1.split[1]
       custom_role_path(user, role)
       
-    when /^the preview of my "([^"]*)" profile/
-      user = $1.split[0]
-      role = $1.split[1]
+    when /^the preview of my tester talent profile/
+      user = 'tester'
+      role = 'talent'
       custom_role_path(user, role) + '?preview=true'
       
     when /my "([^"]*)" "([^"]*)" media collection page for the "([^"]*)" role/
