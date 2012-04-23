d82413ec43b4a7c04161c646b75e56fb5d53b2d8
amyykuo <amyykuo@berkeley.edu>
2012-04-22 18:21:26 -0700

    Youtube bug fixes from Amy's computer...

--- app/models/media_asset.rb
+++ app/models/media_asset.rb
@@ -1,16 +1,10 @@
 include Rack::Utils
-include ActionView::Helpers
-include ActionView::Helpers::AssetTagHelper
 
 class MediaAsset < ActiveRecord::Base
 	belongs_to :media_collection
 	has_attached_file :media, :styles => {:normal => "300x300>", :thumb => "128x128#", :preview => "900x450>"}
 	
 	before_create :process_youtube_id
-	
-	def youtube_embed_frame
-		content_tag(:iframe, nil, :width => 940, :height => 500, :src => "http://www.youtube.com/embed/#{self.youtube_id}", :frameborder => 0, :allowfullscreen => true)
-	end
 	
 	def image?
 		not self.media_file_name.nil?
