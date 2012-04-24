5737eaa53b5f60798d0c62e67e6932ab22851c9a
Amy Kuo <amyykuo@berkeley.edu>
2012-04-23 21:00:08 -0700

    Finished index_controller_spec for this iteration.

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe IndexController do
+  
+  describe 'index' do
+    
+    it "should redirect to user's home page if they're logged in." do
+      @current_user = login_as("Kunal")
+      get :index
+      response.should redirect_to home_path("kunal")
+    end
+    
+    it "should render welcome if user is not logged in" do
+      get :index
+      response.should render_template "index"
+    end
+  end
 
 end
